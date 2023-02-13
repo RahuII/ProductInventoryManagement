6cde8a65517a132bf136d679dd37886b793dda72
Rahul Kumar <r1303yadav@gmail.com>
2023-02-13 19:07:11 +0530

    product class created

---
+++ Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Product
+    {
+        public string name { get; set; }
+        public double price { get; set; }
+        public int quantity { get; set; }
+        public string type { get; set; }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Assignment2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Product> productList = new List<Product>();
+
+            productList.Add(new Product() { name = "Rahul", price = 10.5, quantity = 50, type = "Leafy green" });
+        }
+
+        public static void addProduct()
+        {
+
+        }
+    }
+}
